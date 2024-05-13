a6dd03df51ee73db5cc50064ac6d55b55ae7b7ad
inaraahmadova <tu7r5hs82@code.edu.az>
2024-05-13 20:15:17 +0400

    first commit

---
+++ Pronia/DataAccesLayer/ProniaContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pronia.Models;
+
+namespace Pronia.DataAccesLayer
+{
+    public class ProniaContext : DbContext
+    {
+        public ProniaContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseSqlServer("Server=LAPTOP-ACG39MDK\\SQLEXPRESS;Database=Proniia;Trusted_Connection=true;TrustServerCertificate=True");
+            base.OnConfiguring(options);
+        }
+    }
+}
